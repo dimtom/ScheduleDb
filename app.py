217a7f883e8438c3c4c5e9efebbed7bed70ad986
Dmitry Bimatov <dbimatov@gmail.com>
2021-12-09 18:24:52 -0800

    Basic app - added

---
+++ app.py
@@ -0,0 +1,18 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def handler_main():
+    return "<p>Hello, world!</p>"
+
+
+@app.route("/help")
+def handler_help():
+    return "<p>This is help page</p>"
+
+
+@app.route("/login")
+def handler_login():
+    return "<p>This is login page</p>"
